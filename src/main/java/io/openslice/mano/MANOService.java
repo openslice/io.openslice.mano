363dc276cbb3d6d960a6f820974dc3af8b15c87b
Christos Tranoris <tranoris@gmail.com>
2020-02-07 18:01:53 +0200

    fixing runtime error

--- src/main/java/io/openslice/mano/MANOService.java
+++ src/main/java/io/openslice/mano/MANOService.java
@@ -25,6 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
 
 
 /**
@@ -47,6 +48,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @EnableAutoConfiguration
 // This is enabled by default
 @EnableConfigurationProperties
+@ComponentScan(basePackages = { 
+		"io.openslice.mano",
+		"io.openslice.centrallog.client"	})
 public class MANOService {
 	public static void main(String[] args) {		
 		SpringApplication.run( MANOService.class, args);	
