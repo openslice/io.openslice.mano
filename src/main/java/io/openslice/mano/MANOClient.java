1f80005f50bf7ccf47997fdf9108b1b519266598
Ioannis.chatzis <ioannischatzis@gmail.com>
2021-07-19 22:29:33 +0300

    Starting sync

--- src/main/java/io/openslice/mano/MANOClient.java
+++ src/main/java/io/openslice/mano/MANOClient.java
@@ -571,6 +571,36 @@ public class MANOClient {
 		return dd;
 	}
 
+	public List<MANOprovider> getMANOproviders()
+	{
+		String ret = template.requestBody("activemq:queue:getMANOProviderByID", "", String.class);
+		
+		List<MANOprovider> mps = null;
+		// Map object to ExperimentMetadata
+		try {
+			// Map object to ExperimentMetadata
+			ObjectMapper mapper = new ObjectMapper();
+			logger.info("From ActiveMQ:" + ret.toString());
+			mps = mapper.readValue(ret, new TypeReference<List<MANOprovider>>() {});
+			for (int i = 0; i < mps.size(); i++) {
+				logger.info("Found EndPoint " + mps.get(i).getApiEndpoint());
+			}
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			logger.error(e.getMessage());
+		} catch (JsonMappingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			logger.error(e1.getMessage());
+		} catch (IOException e11) {
+			// TODO Auto-generated catch block
+			e11.printStackTrace();
+			logger.error(e11.getMessage());
+		}
+		return mps;		
+	}
+ 	
 	// Update the data from the portal api (database)
 	public MANOprovider getMANOproviderByID(long id) {
 		String ret = template.requestBody("activemq:queue:getMANOProviderByID", id, String.class);
